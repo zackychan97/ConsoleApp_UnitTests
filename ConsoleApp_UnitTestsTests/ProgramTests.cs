428bf4f2b16ccdfda4b1393f239e07070ce9995b
Zack Bumpous <zackbumpous97@gmail.com>
2024-02-18 21:13:59 -0600

    Adds code to test print random dino

--- ConsoleApp_UnitTestsTests/ProgramTests.cs
+++ ConsoleApp_UnitTestsTests/ProgramTests.cs
@@ -62,7 +62,46 @@ namespace A1_ConsoleApp.Tests
         }
 
         [TestMethod()]
-        public void PrintRandomDinoTest() { }
+        public void PrintRandomDinoTest() 
+        {
+            // Arrange
+            var originalConsoleOut = Console.Out; // Save the original Console.Out
+            var outputBuilder = new StringWriter();
+            Console.SetOut(outputBuilder); // Redirect Console.Out
+            List<string> Dinosaurs = new List<string>();
+            Dinosaurs.Add("Tyrannosaurus Rex");
+            Dinosaurs.Add("Velociraptor");
+            Dinosaurs.Add("Stegosaurus");
+            Dinosaurs.Add("Triceratops");
+            Dinosaurs.Add("Spinosaurus");
+            Dinosaurs.Add("Ankylosaurus");
+            Dinosaurs.Add("Brachiosaurus");
+            Dinosaurs.Add("Dilophosaurus");
+            Dinosaurs.Add("Argentinosaurus");
+            Dinosaurs.Add("Parasaurolophus");
+
+            // Act
+            Program.PrintRandomDino();
+
+            // Assert
+            string expectedOutputStart = "Printing random dino.";
+            string expectedDino = "";
+            string actualOutput = outputBuilder.ToString();
+            foreach (var dino in Dinosaurs) 
+            {
+                if (actualOutput.Contains(dino))
+                {
+                    expectedDino = dino;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(actualOutput.StartsWith(expectedOutputStart));
+            Assert.IsTrue(actualOutput.Contains(expectedDino));
+
+            // Restore the original Console.Out
+            Console.SetOut(originalConsoleOut);
+        }
 
         [TestMethod()]
         public void PrintRandomStringActionTest() { }
